60904ee941ad3aa953c7ec5eb36041e0037baafd
Lorenzo_Senesi <8293319@itisrossi.vi.it>
2024-02-26 17:24:55 +0100

    funzioni di login e register

--- scripts/base_script.js
+++ scripts/base_script.js
@@ -1,3 +1,5 @@
+// ---------------- barra del menu laterale ---------------------------------------
+
 function toggleSidebar() {
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -6,7 +8,7 @@ function closeSidebar() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-
+// ---------------- Menu utente dropdown -----------------------------------------
 
 function toggleDropdown() {
     var dropdownMenu = document.getElementById("dropdownMenu");
@@ -26,3 +28,39 @@ window.addEventListener("click", function(event) {
         dropdownMenu.style.display = "none";
     }
 });
+
+// ---------------- Chiamata controllo login ---------------------------------------
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../query/Utenti_login.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText); // Puoi gestire la risposta come desideri
+        }
+    };
+    xhr.send("username=" + username + "&password=" + password);
+}
+
+// ---------------- Chiamata controllo registrazione ---------------------------------------
+
+function register() {
+    var nome = document.getElementById("nome").value;
+    var cognome = document.getElementById("cognome").value;
+    var regUsername = document.getElementById("regUsername").value;
+    var regPassword = document.getElementById("regPassword").value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../query/Utenti_register.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText); // Puoi gestire la risposta come desideri
+        }
+    };
+    xhr.send("nome=" + nome + "&cognome=" + cognome + "&username=" + regUsername + "&password=" + regPassword);
+}
